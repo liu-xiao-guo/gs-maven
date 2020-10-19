0cbcf4246ab82feb3178a5d6b100dc68f03cf077
Xiaoguo Liu <liu_xiao_guo@yahoo.com>
2020-10-19 12:07:59 +0800

    Added some code using log4j2 to produce some logs

--- complete/src/main/java/hello/HelloWorld.java
+++ complete/src/main/java/hello/HelloWorld.java
@@ -1,12 +1,18 @@
 package hello;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
+	private static final Logger logger = LogManager.getLogger(HelloWorld.class);
+	
 	public static void main(String[] args) {
 		LocalTime currentTime = new LocalTime();
 		System.out.println("The current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
+		
+		logger.error("Application is running!");		
 	}
 }
